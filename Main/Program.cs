c979a1f5c12d131e166b0737cc560ddbf35eda4b
Alessandro Palladino <alessandro.palladino@artea.com>
2022-09-07 17:27:53 +0200

    Add Recursive Check in Class

--- Main/Program.cs
+++ Main/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
@@ -10,15 +11,22 @@ namespace Main
     class Program
     {
 
-        class simple
+        class ClassA
         {
             public int a { get; set; }
         }
-        class twostring
+        class ClassTwoStr
         {
             public string a { get; set; }
             public string b { get; set; }
         }
+
+        class ClassR
+        {
+            public string a { get; set; }
+            public ClassR R { get; set; }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Size int :{0}", TestSize<int>.SizeOf(1));
@@ -41,19 +49,35 @@ namespace Main
             Console.WriteLine("Size stringbulder U[10*100] :{0}", TestSize<StringBuilder>.SizeOf(sb));
 
 
-            Console.WriteLine("Size C:simple :{0}", TestSize<simple>.SizeOf(new simple()));
-            Console.WriteLine("Size C:strings :{0}", TestSize<twostring>.SizeOf(new twostring()));
-            Console.WriteLine("Size C:strings[setted]:{0}", TestSize<twostring>.SizeOf(new twostring() { a = "0123456789", b = "0123456789" }));
+            Console.WriteLine("Size C:simple :{0}", TestSize<ClassA>.SizeOf(new ClassA()));
+            Console.WriteLine("Size C:strings :{0}", TestSize<ClassTwoStr>.SizeOf(new ClassTwoStr()));
+            Console.WriteLine("Size C:strings[setted]:{0}", TestSize<ClassTwoStr>.SizeOf(new ClassTwoStr() { a = "0123456789", b = "0123456789" }));
             var arrayint = new int[100];
             Console.WriteLine("Size arrayint[100] :{0}", TestSize<int[]>.SizeOf(arrayint));
 
-            var arraysimple = new simple[3];
-            Console.WriteLine("Size simple[3] :{0}", TestSize<simple[]>.SizeOf(arraysimple));
+            var arraysimple = new ClassA[3];
+            Console.WriteLine("Size simple[3] :{0}", TestSize<ClassA[]>.SizeOf(arraysimple));
 
             var list = new List<int>();
             Console.WriteLine("Size list Empty :{0}", TestSize<List<int>>.SizeOf(list));
             for (int i = 0; i < 100; i++) list.Add(i);
             Console.WriteLine("Size list<int>[100] :{0}", TestSize<List<int>>.SizeOf(list));
+
+            var dict = Enumerable.Range(0, 100).ToDictionary(x => x, x => new string('a', x));
+
+            
+            Console.WriteLine("Size Dictonary<int,string>[100] :{0}", TestSize<Dictionary<int,string>>.SizeOf(dict));
+            var real = dict.Keys.Count * System.Runtime.InteropServices.Marshal.SizeOf(typeof(int)) + dict.Values.Sum(x => Encoding.Default.GetByteCount(x));
+            Console.WriteLine("Size Dictonary<int,string>[100] :{0}", real);
+
+            var classR_top = new ClassR()
+            {
+                a = "Thisis a string",
+                R = new ClassR()
+            };
+            classR_top.R.R = classR_top;
+
+            Console.WriteLine("Size Class Recursive :{0}", TestSize<ClassR>.SizeOf(classR_top));
 
             Console.ReadLine();
         }
@@ -62,7 +86,24 @@ namespace Main
     class TestSize<T>
     {
 
-        static private int SizeOfObj(Type T, object thevalue)
+        
+        static private int SizeOfClass(object thevalue, ObjectIDGenerator gen)
+        {
+            gen.GetId(thevalue, out bool isfirstTime);
+            if (!isfirstTime) return 0;
+            var fields = thevalue.GetType().GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            int returnval = 0;
+            for (int i = 0; i < fields.Length; i++)
+            {
+                Type t = fields[i].FieldType;
+                Object v = fields[i].GetValue(thevalue);
+                returnval += 4 + SizeOfObj(t, v, gen);
+            }
+            return returnval;
+        }
+        
+        static private int SizeOfObj(Type T, object thevalue, ObjectIDGenerator gen)
         {
             var type = T;
             int returnval = 0;
@@ -97,15 +138,9 @@ namespace Main
                 }
                 catch { }
             }
-            else
+            else if(type.IsClass)
             {
-                var fields = type.GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                for (int i = 0; i < fields.Length; i++)
-                {
-                    Type t = fields[i].FieldType;
-                    Object v = fields[i].GetValue(thevalue);
-                    returnval += 4 + SizeOfObj(t, v);
-                }
+                returnval += SizeOfClass(thevalue, gen ?? new ObjectIDGenerator());
             }
             if (returnval == 0)
                 try
@@ -117,7 +152,7 @@ namespace Main
         }
         static public int SizeOf(T value)
         {
-            return SizeOfObj(typeof(T), value);
+            return SizeOfObj(typeof(T), value, null);
         }
     }
 }
